edfb1273975864d16394c1ef969fccff791f3b7f
aljustiet <aljustiet@tutamail.com>
2024-12-03 22:57:59 +0500

    Copy hypr config directory from main to local

--- hypr/gamemode.sh
+++ hypr/gamemode.sh
@@ -3,7 +3,8 @@ HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
     hyprctl --batch "\
         keyword animations:enabled 0;\
-        keyword decoration:drop_shadow 0;\
+        keyword decoration:drop_shadow false;\
+        keyword decoration:dim_inactive false;\
         keyword decoration:blur:enabled 0;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
