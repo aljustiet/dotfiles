1fea17da2d4ad8489d295e5b08237ceaaf57fbc5
aljustiet <aljustiet@tutamail.com>
2024-12-04 17:56:20 +0500

    Add telegram brew cask

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -73,7 +73,7 @@
             "cog"
             "middleclick"
             "nuclear"
-            
+            "telegram"
           ];
           masApps = {
             "Plain Text Editor" = 1572202501;
