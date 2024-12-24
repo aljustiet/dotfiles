66b2aa54ccf3116333b96be20c2e48fe705d518a
aljustiet <aljustiet@tutamail.com>
2024-12-24 16:27:39 +0500

    Checkpointing

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,51 +1,3 @@
-vim.g.neovide_remember_window_size = true
-
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-vim.g.mapleader = " "
-
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-local lazy_config = require "configs.lazy"
-
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
-  },
-  {
-    "andrewferrier/wrapping.nvim",
-    lazy = false,
-    config = function()
-    require("wrapping").setup()
-    end
-  },
-  { import = "plugins" },
-}, lazy_config)
-
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
-require "nvchad.autocmds"
-
-vim.schedule(function()
-  require "mappings"
-end)
-
 -- Set no highlight search by default
 vim.o.hlsearch = false
 
@@ -65,9 +17,11 @@ keymap('n', 's', 'a', opts)
 keymap('n', 'f', 'd', opts)
 keymap('n', 'l', 'e', opts)
 keymap('n', 'm', 'x', opts)
-keymap('v', 'm', 'x', opts)
 keymap('n', 'i', 'l', opts)
 keymap('n', 'a', 'j', opts)
+keymap('n', 'c', 'm', opts)
+keymap('n', ';', ':', opts)
+keymap('v', 'm', 'x', opts)
 keymap('v', 'i', 'l', opts)
 keymap('v', 'a', 'j', opts)
 keymap('v', 'e', 'k', opts)
@@ -82,5 +36,3 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     pattern = {"*.yuck", "*.kbd"},
     command = "set filetype=lisp"
 })
-
--- vim.o.wrap = false
