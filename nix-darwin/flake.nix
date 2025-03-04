0dacb5871e7444c053092fadecc1d1e6829c4415
aljustiet <aljustiet@tutamail.com>
2025-03-04 09:41:01 +0500

    Updating

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,7 +2,7 @@
   description = "Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+#    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url= "github:zhaofengli/nix-homebrew";
@@ -52,6 +52,7 @@
           glow
           geogebra6
           jdk23
+          tree
         ];
 
         homebrew = {
@@ -89,6 +90,7 @@
             "handbrake"
             "discord"
             "android-studio"
+            "intellij-idea"
           ];
 
           masApps = {
@@ -133,7 +135,7 @@
         dock.autohide = true;
       };
 
-      services.nix-daemon.enable = true;
+#      services.nix-daemon.enable = true;
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
