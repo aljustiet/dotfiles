f021351aee6c16de981a33df530345f19d24f405
aljustiet <aljustiet@tutamail.com>
2025-04-18 13:59:10 +0500

    Hyprland update after long time

--- hypr/scripts/gamemode.sh
+++ hypr/scripts/gamemode.sh
@@ -5,6 +5,7 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword animations:enabled 0;\
         keyword decoration:inactive_opacity 1;\
         keyword decoration:dim_inactive false;\
+        keyword decoration:dim_active false;\
         keyword decoration:blur:enabled false;\
         keyword decoration:shadow:enabled false;\
         keyword general:gaps_in 0;\
