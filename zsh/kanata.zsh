d23ffb87af091a4431630474925bbd7298ee0832
aljustiet <aljustiet@tutamail.com>
2025-08-06 17:31:37 +0500

    Restart kanata instead of starting systemd

--- zsh/kanata.zsh
+++ zsh/kanata.zsh
@@ -1,3 +1,3 @@
-alias rk="systemctl --user start kanata.service"
+alias rk="systemctl --user restart kanata.service"
 alias kk="systemctl --user stop kanata.service"
 alias djfk="journalctl --user -feu kanata.service"
