cabd9c423b8a4839bfcea702893c1eddd1a03dc4
aljustiet <aljustiet@tutamail.com>
2025-08-15 02:24:09 +0500

    Checkpoint

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
+set fish_greeting
 # Environment variables
 set -gx LESS "--ignore-case --quit-if-one-screen --no-init --RAW-CONTROL-CHARS"
 set -gx PATH "$PATH:/home/aljustiet/.local/bin"
