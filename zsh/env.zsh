494912f560b7fb3701d6dd033626affd57a46fcf
aljustiet <aljustiet@tutamail.com>
2025-08-23 03:32:53 +0500

    Checkpoint

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -5,12 +5,7 @@ export QT_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
 
 # Path
-export PATH=$PATH:/home/aljustiet/Documents/platform-tools
-export PATH=$PATH:/home/aljustiet/go/bin
-export PATH=/home/aljustiet/.cargo/bin:$PATH
-export PATH=/home/aljustiet/.local/bin:$PATH
-export PATH=/usr/bin/vendor_perl:$PATH
-export PATH=/usr/bin/site_perl:$PATH
+
 
 # Environment variables
 #export TIMEFMT=$'\n================\nCPU\t%P\nuser\t%*U\nsystem\t%*S\ntotal\t%*E'
