60b2e39c77e183323c008ae916b787905fc3423f
aljustiet <aljustiet@tutamail.com>
2025-09-15 15:39:18 +0500

    add archive

--- zshrc/nixos.zsh
+++ zshrc/nixos.zsh
@@ -1,9 +1,10 @@
-alias rebuild="doas nixos-rebuild switch --flake /etc/nixos"
-alias en="doas nvim /etc/nixos/thinkpad-nixos-configuration.nix"
+alias rebuild="nixos-rebuild switch --flake ~/nix --sudo"
+alias en="nvim ~/nix/thinkpad-nixos-configuration.nix"
 alias rh="home-manager switch"
-alias r="doas nixos-rebuild switch"
+alias r="nixos-rebuild switch --flake ~/nix --sudo"
 alias eh="nvim ~/.config/home-manager/home.nix"
-alias uf="doas nix flake update --flake /etc/nixos"
+alias uf="nix flake update --flake ~/nix"
+alias ef="nvim ~/nix/flake.nix"
 cg() {
     doas nix-collect-garbage -d
     nix-collect-garbage -d
