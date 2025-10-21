481742c296c0007510a604cd31a9abd6c9e4c0f8
aljustiet <aljustiet@tutamail.com>
2025-10-21 12:30:07 +0500

    update

--- zshrc/nixos.zsh
+++ zshrc/nixos.zsh
@@ -2,11 +2,11 @@ alias rebuild="nixos-rebuild switch --flake ~/nix --sudo"
 alias en="nvim ~/nix/configuration.nix"
 alias rh="home-manager switch --flake ~/nix/home/"
 alias r="nixos-rebuild switch --flake ~/nix --sudo"
-alias eh="nvim ~/nix/home-manager/home.nix"
+alias eh="nvim ~/nix/home/home.nix"
 alias ef="nvim ~/nix/flake.nix"
 uf() {
     nix flake update --flake ~/nix
-    nix flake update --flake ~/nix/home-manager
+    nix flake update --flake ~/nix/home
 }
 cg() {
     sudo nix-collect-garbage -d
