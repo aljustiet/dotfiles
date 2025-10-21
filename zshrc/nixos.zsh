d10b5959061fd300eada04b9d39eb9519f70fc94
aljustiet <aljustiet@tutamail.com>
2025-10-21 11:49:01 +0500

    update

--- zshrc/nixos.zsh
+++ zshrc/nixos.zsh
@@ -1,6 +1,6 @@
 alias rebuild="nixos-rebuild switch --flake ~/nix --sudo"
 alias en="nvim ~/nix/configuration.nix"
-alias rh="home-manager switch --flake ~/nix/home-manager/"
+alias rh="home-manager switch --flake ~/nix/home/"
 alias r="nixos-rebuild switch --flake ~/nix --sudo"
 alias eh="nvim ~/nix/home-manager/home.nix"
 alias ef="nvim ~/nix/flake.nix"
