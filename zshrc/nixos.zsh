47cccd5ece7a424f932d1f668b1b7ab22fd3463c
aljustiet <aljustiet@tutamail.com>
2025-11-09 19:26:16 +0500

    make home manager builds verbose

--- zshrc/nixos.zsh
+++ zshrc/nixos.zsh
@@ -1,6 +1,6 @@
 alias rebuild="nixos-rebuild switch --flake ~/nix --sudo"
 alias en="nvim ~/nix/configuration.nix"
-alias rh="home-manager switch --flake ~/nix/home/"
+alias rh="home-manager switch --flake ~/nix/home/ --verbose"
 alias r="nixos-rebuild switch --flake ~/nix --sudo --verbose |& tee ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log"
 alias rb="nixos-rebuild boot --flake ~/nix --sudo --verbose |& tee ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log"
 alias eh="nvim ~/nix/home/home.nix"
