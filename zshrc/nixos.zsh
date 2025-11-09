cf1eacaa611fe4fe428f444d7559982c7ccfd217
aljustiet <aljustiet@tutamail.com>
2025-11-09 19:55:52 +0500

    wrap in script r alias

--- zshrc/nixos.zsh
+++ zshrc/nixos.zsh
@@ -1,8 +1,8 @@
 alias rebuild="nixos-rebuild switch --flake ~/nix --sudo"
 alias en="nvim ~/nix/configuration.nix"
 alias rh="home-manager switch --flake ~/nix/home/ --verbose"
-alias r="nixos-rebuild switch --flake ~/nix --sudo --verbose --max-jobs 16 |& tee ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log"
-alias rb="nixos-rebuild boot --flake ~/nix --sudo --verbose |& tee ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log"
+alias r='script -q -c "nixos-rebuild switch --flake ~/nix --sudo --verbose --max-jobs 16" ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log'
+alias rb='script -q -c "nixos-rebuild boot --flake ~/nix --sudo --verbose --max-jobs 16" ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log'
 alias eh="nvim ~/nix/home/home.nix"
 alias ef="nvim ~/nix/flake.nix"
 uf() {
