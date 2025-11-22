b8a82fdb1185f06dce2bdaa061859a0ca48de02c
aljustiet <aljustiet@tutamail.com>
2025-11-22 11:45:43 +0500

    remove alias for starting waybar rw

--- zshrc/niri.zsh
+++ zshrc/niri.zsh
@@ -3,7 +3,6 @@ nmas() {
     niri msg action spawn -- "dash" "-c" "$*"
 }
 alias nv="niri validate"
-alias rw="niri msg action spawn -- 'waybar'"
 alias nsr="niri-screen-time"
 alias nw="niri msg windows"
 alias na='niri msg windows | grep "App ID:"'
@@ -11,4 +10,5 @@ alias mm="niri msg action move-workspace-to-monitor"
 alias mi="niri msg action move-workspace-to-index"
 alias eb="nvim ~/.config/niri/binds.kdl"
 alias enc="nvim ~/.config/niri/config.kdl"
+alias rw="pkill waybar && niri msg action spawn -- 'waybar'"
 #alias eo='niri msg output eDP-1 on
