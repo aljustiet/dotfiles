0603f786d8166cf546438235a9df404b0d0e4c3c
aljustiet <aljustiet@tutamail.com>
2025-11-23 15:09:02 +0500

    remove mini ai

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,8 +38,8 @@ vim.keymap.set('n', 'T', 'I')
 vim.keymap.set('n', 'S', 'A')
 vim.keymap.set('n', 'a', 'gj')
 vim.keymap.set('n', 'e', 'gk')
-vim.keymap.set('v', 'a', 'gj')
-vim.keymap.set('v', 'e', 'gk')
+vim.keymap.set('x', 'a', 'gj')
+vim.keymap.set('x', 'e', 'gk')
 vim.keymap.set('n', 'i', 'l')
 vim.keymap.set('n', 'm', 'x')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
@@ -761,44 +761,7 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
-  },
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
