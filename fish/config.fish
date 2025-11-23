646b1478a2fe9a8740f6a2233c24ed663fb5de49
aljustiet <aljustiet@tutamail.com>
2025-11-23 17:54:42 +0500

    add y alias for yazi integration with fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
