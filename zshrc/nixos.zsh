685a4d54cf89c3fa4a02ac5d821574651eefa420
aljustiet <aljustiet@tutamail.com>
2025-12-01 10:46:14 +0500

    make nixos rebuild non verbose

--- zshrc/nixos.zsh
+++ zshrc/nixos.zsh
@@ -1,8 +1,8 @@
 alias rebuild="nixos-rebuild switch --flake ~/nix --sudo"
 alias en="nvim ~/nix/configuration.nix"
-alias rh='script -q -c "home-manager switch --flake ~/nix/home --verbose --max-jobs 16" ~/nix/home/logs/home-manager-$(date +%Y-%m-%d_%H-%M-%S).log'
-alias r='script -q -c "nixos-rebuild switch --flake ~/nix --sudo --verbose --max-jobs 16" ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log'
-alias rb='script -q -c "nixos-rebuild boot --flake ~/nix --sudo --verbose --max-jobs 16" ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log'
+alias rh='script -q -c "home-manager switch --flake ~/nix/home --max-jobs 16" ~/nix/home/logs/home-manager-$(date +%Y-%m-%d_%H-%M-%S).log'
+alias r='script -q -c "nixos-rebuild switch --flake ~/nix --sudo --max-jobs 16" ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log'
+alias rb='script -q -c "nixos-rebuild boot --flake ~/nix --sudo --max-jobs 16" ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log'
 alias eh="nvim ~/nix/home/home.nix"
 alias ef="nvim ~/nix/flake.nix"
 uf() {
