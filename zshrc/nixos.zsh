371fba6000a09ba8141398c908ad5d7bb6680d64
aljustiet <aljustiet@tutamail.com>
2025-12-03 14:28:20 +0500

    remove script from nixos rebuild alias

--- zshrc/nixos.zsh
+++ zshrc/nixos.zsh
@@ -1,8 +1,8 @@
 alias rebuild="nixos-rebuild switch --flake ~/nix --sudo"
 alias en="nvim ~/nix/configuration.nix"
-alias rh='script -q -c "home-manager switch --flake ~/nix/home --max-jobs 16" ~/nix/home/logs/home-manager-$(date +%Y-%m-%d_%H-%M-%S).log'
-alias r='script -q -c "nixos-rebuild switch --flake ~/nix --sudo --max-jobs 16" ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log'
-alias rb='script -q -c "nixos-rebuild boot --flake ~/nix --sudo --max-jobs 16" ~/nix/logs/nixos-rebuild-$(date +%Y-%m-%d_%H-%M-%S).log'
+alias rh="home-manager switch --flake ~/nix/home --max-jobs 16"
+alias r="nixos-rebuild switch --flake ~/nix --sudo --max-jobs 16"
+alias rb="nixos-rebuild boot --flake ~/nix --sudo --max-jobs 16"
 alias eh="nvim ~/nix/home/home.nix"
 alias ef="nvim ~/nix/flake.nix"
 uf() {
