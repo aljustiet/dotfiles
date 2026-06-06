d713a982efb45a2011ad3d4b23d1e47e7490a61c
aljustiet <aljustiet@tutamail.com>
2026-06-06 08:18:15 +0500

    notify-send only title for r alias nixos

--- zshrc/nixos.zsh
+++ zshrc/nixos.zsh
@@ -1,7 +1,7 @@
 alias rebuild="nixos-rebuild switch --flake ~/nix --sudo"
 alias en="nvim ~/nix/configuration.nix"
 alias rh="home-manager switch --flake ~/nix/home --max-jobs 16"
-alias r="nixos-rebuild switch --flake ~/nix --sudo --max-jobs 16 || notify-send Build Done"
+alias r="nixos-rebuild switch --flake ~/nix --sudo --max-jobs 16 || notify-send 'Build Done'"
 alias rb="nixos-rebuild boot --flake ~/nix --sudo --max-jobs 16"
 alias eh="nvim ~/nix/home/home.nix"
 alias ef="nvim ~/nix/flake.nix"
